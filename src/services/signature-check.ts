0a18b5b378f72ff7891bbbb9add2ff7972038867
Paul.Benn-Darias <set>
2021-03-03 15:17:06 +0000

    checkSignature should return void

--- src/services/signature-check.ts
+++ src/services/signature-check.ts
@@ -1,11 +1,11 @@
 import * as JWT from "jsonwebtoken";
 import {getCertificateChain} from "./azure";
 
-export const checkSignature = async (token: any): Promise<string | object> => {
+export const checkSignature = async (token: any): Promise<void> => {
   // tid = tenant ID
   const certificate = await getCertificateChain(token.payload.tid, token.header.kid);
 
-  return JWT.verify(
+  JWT.verify(
     token,
     certificate,
     {
