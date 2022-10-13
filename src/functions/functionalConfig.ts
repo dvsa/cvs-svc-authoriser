fa58b2751608a5aaf470884274ca3ae9ce142518
Steven.Pears <steven.pears@live.co.uk>
2022-10-13 14:34:34 +0100

    extract functional config to its own file

---
+++ src/functions/functionalConfig.ts
@@ -0,0 +1,65 @@
+import { HttpVerb } from "../services/http-verbs";
+export type NonEmptyArray<T> = [T, ...T[]];
+export interface IApiAccess {
+    verb: HttpVerb;
+    path: string;
+}
+
+export const functionConfig: { [key: string]: NonEmptyArray<IApiAccess> } = {
+    "TechRecord.Amend": [
+        {
+            verb: "POST",
+            path: "vehicles/*",
+        },
+        {
+            verb: "PUT",
+            path: "vehicles/*",
+        },
+    ],
+    "TechRecord.View": [
+        {
+            verb: "GET",
+            path: "vehicles/*",
+        },
+    ],
+    "TestResult.Amend": [
+        {
+            verb: "POST",
+            path: "test-result/*",
+        },
+        {
+            verb: "PUT",
+            path: "test-result/*",
+        },
+        {
+            verb: "GET",
+            path: "test-types/*",
+        },
+        {
+            verb: "GET",
+            path: "test-stations/*",
+        },
+        {
+            verb: "GET",
+            path: "defects/*",
+        },
+        {
+            verb: "GET",
+            path: "reference/*",
+        },
+    ],
+    "TestResult.View": [
+        {
+            verb: "GET",
+            path: "test-result/*",
+        },
+        {
+            verb: "GET",
+            path: "test-types/*",
+        },
+        {
+            verb: "GET",
+            path: "v1/document-retrieval/*",
+        },
+    ]
+};
--- src/functions/functionalPolicyFactory.ts
+++ src/functions/functionalPolicyFactory.ts
@@ -1,73 +1,8 @@
 import { APIGatewayAuthorizerResult, Statement } from "aws-lambda";
 import newPolicyDocument from "./newPolicyDocument";
 import { ILogEvent } from "../models/ILogEvent";
-import { HttpVerb } from "../services/http-verbs";
 import StatementBuilder from "../services/StatementBuilder";
-
-type NonEmptyArray<T> = [T, ...T[]];
-
-interface IApiAccess {
-  verb: HttpVerb;
-  path: string;
-}
-
-const functionConfig: { [key: string]: NonEmptyArray<IApiAccess> } = {
-  "TechRecord.Amend": [
-    {
-      verb: "POST",
-      path: "vehicles/*",
-    },
-    {
-      verb: "PUT",
-      path: "vehicles/*",
-    },
-  ],
-  "TechRecord.View": [
-    {
-      verb: "GET",
-      path: "vehicles/*",
-    },
-  ],
-  "TestResult.Amend": [
-    {
-      verb: "POST",
-      path: "test-result/*",
-    },
-    {
-      verb: "PUT",
-      path: "test-result/*",
-    },
-    {
-      verb: "GET",
-      path: "test-types/*",
-    },
-    {
-      verb: "GET",
-      path: "test-stations/*",
-    },
-    {
-      verb: "GET",
-      path: "defects/*",
-    },
-    {
-      verb: "GET",
-      path: "reference/*",
-    },
-  ],
-  "TestResult.View": [
-    {
-      verb: "GET",
-      path: "test-result/*",
-    },
-    {
-      verb: "GET",
-      path: "test-types/*",
-    },
-    {
-      verb: "GET",
-      path: "v1/document-retrieval/*",
-    },
-};
+import { functionConfig, IApiAccess } from "./functionalConfig";
 
 function toStatement(access: IApiAccess): Statement {
   return new StatementBuilder().setEffect("Allow").setHttpVerb(access.verb).setResource(access.path).build();
