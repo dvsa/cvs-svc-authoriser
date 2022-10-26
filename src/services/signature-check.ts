6d8ebf350eaa397a536507b78f8910bd27299887
Steven.Pears <steven.pears@live.co.uk>
2022-10-26 10:16:50 +0100

    remove any type from checkSignature

--- src/services/signature-check.ts
+++ src/services/signature-check.ts
@@ -1,9 +1,9 @@
 import * as JWT from "jsonwebtoken";
 import { getCertificateChain } from "./azure";
 
-export const checkSignature = async (encodedToken: string, decodedToken: any, tenantId: string, clientId: string): Promise<void> => {
+export const checkSignature = async (encodedToken: string, decodedToken: JWT.Jwt, tenantId: string, clientId: string): Promise<void> => {
   // tid = tenant ID, kid = key ID
-  const certificate = await getCertificateChain(tenantId, decodedToken.header.kid);
+  const certificate = await getCertificateChain(tenantId, decodedToken.header.kid as string);
 
   JWT.verify(encodedToken, certificate, {
     audience: clientId,
