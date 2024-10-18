7112643d67128281ff85f1dea400ef3379bc5571
Pete Mallam <pete.mallam@bjss.com>
2024-10-18 13:27:21 +0100

    Add more logging

--- src/services/azure.ts
+++ src/services/azure.ts
@@ -16,7 +16,7 @@ export const getCertificateChain = async (tenantId: string, keyId: string): Prom
 
 const getKeys = async (tenantId: string): Promise<Map<string, string>> => {
   const response = await fetchKeys(tenantId);
-
+  console.log("Public Keys Read")
   const map: Map<string, string> = new Map();
 
   const resp: KeyResponse = await response.json();
@@ -27,10 +27,11 @@ const getKeys = async (tenantId: string): Promise<Map<string, string>> => {
 
     map.set(keyId, certificateChain);
   }
+  console.log("Key Map Created")
   return map;
 };
 
 export const fetchKeys = (tenantId: string) => {
-  console.log("Fetching keys");
+  console.log("Fetching keys from https://login.microsoftonline.com/${tenantId}/discovery/keys");
   return fetch(`https://login.microsoftonline.com/${tenantId}/discovery/keys`);
 };
